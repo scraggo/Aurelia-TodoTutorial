cc33acc224b080ae97b4fb500000d50512368254
Dave Cohen <dcohen18@gmail.com>
2017-07-21 12:40:00 -0500

    finalize
    remove unnecessary files
    include bootstrap 3

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
-export class App {
-  configureRouter(config, router) {
-    config.title = 'Aurelia';
-    config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
-    ]);
+import {Todo} from './todo';
 
-    this.router = router;
+export class App {
+  constructor() {
+    this.heading = "Todos";
+    this.todos = [];
+    this.todoDescription = '';
+  }
+
+  addTodo() {
+    if (this.todoDescription) {
+      this.todos.push(new Todo(this.todoDescription));
+      this.todoDescription = '';
+    }
+  }
+
+  removeTodo(todo) {
+    let index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
   }
 }
